e46d097c986b2759c68029aacf446d75d86dbcf0
Dinitha Sayuranga <133567428+DinieMobo@users.noreply.github.com>
2025-11-09 17:15:25 +0530

    Implemented ENV and Vercel Configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: process.env.VITE_API_URL || "http://localhost:8800",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
